faaf7d033b3e21f9966d2976df220f7c3d247e10
Regulus Black <singhdschauhan10@gmail.com>
2026-01-16 10:00:08 -0500

    removed everything: a fresh start

--- components/quote.tsx
+++
@@ -1,9 +0,0 @@
-export default function Quote({ children }: { children: React.ReactNode; }) {
-	return (
-		<div className="p-16 text-right">
-			<h2 className="text-5xl font-semibold text-slate-700">
-				{children}
-			</h2>
-		</div>
-	);
-}
